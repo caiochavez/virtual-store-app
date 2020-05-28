d4760cd0f80db5692f341bdb2cb0a830161fd13a
caiochavez <caaiochavez@gmail.com>
2020-05-28 18:31:42 -0300

    order screen - list order in progress

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/Home.dart';
+import 'package:loja_virtual/tabs/Orders.dart';
 import 'package:loja_virtual/tabs/Products.dart';
 import 'package:loja_virtual/widgets/CartButton.dart';
 import 'package:loja_virtual/widgets/CustomDrawer.dart';
@@ -26,6 +27,15 @@ class HomeScreen extends StatelessWidget {
           ),
           drawer: CustomDrawer(_pageController),
           body: ProductsTab(),
+        ),
+        Container(color: Colors.yellow),
+        Scaffold(
+          appBar: AppBar(
+            title: Text('Meus Pedidos'),
+            centerTitle: true,
+          ),
+          body: OrdersTab(),
+          drawer: CustomDrawer(_pageController),
         )
       ],
     );
---
+++ lib/tabs/Orders.dart
@@ -0,0 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/screens/Login.dart';
+import 'package:loja_virtual/store/User.dart';
+import 'package:loja_virtual/widgets/OrderTile.dart';
+
+class OrdersTab extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    
+    if (UserStore.of(context).isLoggedIn()) {
+      String uid = UserStore.of(context).firebaseUser.uid;
+      return FutureBuilder<QuerySnapshot>(
+        future: Firestore.instance.collection('users').document(uid).collection('orders').getDocuments(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return Center(child: CircularProgressIndicator());
+          else {
+            return ListView(
+              children: snapshot.data.documents.map((doc) => OrderTile(doc.documentID)).toList(),
+            );
+          }
+        }
+      );
+    } else {
+      return Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.view_list, size: 80, color: Theme.of(context).primaryColor),
+            SizedBox(height: 16),
+            Text(
+              'Faça login para acompanhar os pedidos!',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center
+            ),
+            SizedBox(height: 16),
+            RaisedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => LoginScreen())
+                );
+              },
+              child: Text('Entrar', style: TextStyle(fontSize: 18)),
+              textColor: Colors.white,
+              color: Theme.of(context).primaryColor
+            )
+          ],
+        ),
+      );
+    }
+
+  }
+
+}
