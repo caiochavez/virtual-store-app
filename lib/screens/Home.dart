87bd07666be1b580a66b84abc06644549645a758
caiochavez <caaiochavez@gmail.com>
2020-05-29 16:20:08 -0300

    places screen

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/Home.dart';
 import 'package:loja_virtual/tabs/Orders.dart';
+import 'package:loja_virtual/tabs/Places.dart';
 import 'package:loja_virtual/tabs/Products.dart';
 import 'package:loja_virtual/widgets/CartButton.dart';
 import 'package:loja_virtual/widgets/CustomDrawer.dart';
@@ -28,7 +29,14 @@ class HomeScreen extends StatelessWidget {
           drawer: CustomDrawer(_pageController),
           body: ProductsTab(),
         ),
-        Container(color: Colors.yellow),
+        Scaffold(
+          appBar: AppBar(
+            title: Text('Lojas'),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(_pageController),
+          body: PlacesTab(),
+        ),
         Scaffold(
           appBar: AppBar(
             title: Text('Meus Pedidos'),
